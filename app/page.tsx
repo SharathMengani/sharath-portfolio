fc78d800e4440a74ef1bb2b2b5e348cb0808898a
SharathMengani <chanduvarma57@gmail.com>
2026-05-20 16:58:47 +0530

    .env removed

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
-import Image from "next/image";
 
 export default function Home() {
   return (
